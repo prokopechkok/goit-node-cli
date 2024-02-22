a15f61722ab370cd59606bb259a04b258637502c
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2024-02-22 14:11:22 +0200

    console in table

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       const allContacts = await contactsService.listContacts();
-      return console.log(allContacts);
+      return console.table(allContacts);
 
     case "get":
       const oneContact = await contactsService.getContactById(id);
